6b17ce6fb87165b647391b8c9f9bb9c36bf19084
Carmen Stockberger <carmen.stockberger@gmail.com>
2017-09-02 01:12:32 -0700

    added style to css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,7 +11,7 @@ var score = 0
 //display random target number for user to match between 19 - 120
 var randomNumber = parseInt(Math.floor(Math.random() * (120 - 19) + 19));
 console.log(randomNumber)
-$("#targetNumber").html(randomNumber)
+$("#targetNumber").text(randomNumber)
 
 //assign each crystal random number between 1 - 12
 var gemNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
